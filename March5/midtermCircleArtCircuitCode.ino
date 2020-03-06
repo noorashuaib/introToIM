4fe595b4c4e967fa6cdac14db4c62a2de7daaa2a
noorashuaib <60654412+noorashuaib@users.noreply.github.com>
2020-03-06 12:26:45 +0400

    Update midtermCircleArtCircuitCode.ino

--- March5/midtermCircleArtCircuitCode.ino
+++ March5/midtermCircleArtCircuitCode.ino
@@ -15,13 +15,13 @@ const int greenState = 0;
 const int pos = 0;
 /* integers for the potentiometer */
 const int potentiometer = 0;
-const int val; 
+const int val;
 const int smallDelay = 15;
 
 void setup() {
   /* servoPaper refers to the servo attached to the canvas, the other
-  coloured servos connect to the servos controlling the different
-  markers */
+    coloured servos connect to the servos controlling the different
+    markers */
   pinMode(redButton, INPUT);
   pinMode(blueButton, INPUT);
   pinMode(greenButton, INPUT);
@@ -32,22 +32,22 @@ void setup() {
 }
 
 void loop() {
-  
-  /* the digitalReads make the markers only move when you press a 
-  button */
+
+  /* the digitalReads make the markers only move when you press a
+    button */
   redState = digitalRead(redButton);
   blueState = digitalRead(blueButton);
   greenState = digitalRead(greenButton);
-  
+
   /* first, the servo connected to the canvas (paper) is controlled using the potentiometer */
-  
+
   val = analogRead(potentiometer);
   val = map(val, 0, 1023, 0, 180);
   servoPaper.write(val);
   delay(smallDelay);
 
   /* These commands are for the servos controlled with push buttons. The if statement within the for loop makes the servo only move when the button is pressed.*/
-  
+
   for (pos = 0; pos <= 180; pos += 1) {
     if (redState == HIGH) {
       servoRed.write(pos);
@@ -60,14 +60,14 @@ void loop() {
       servoBlue.write(pos);
       delay(smallDelay);
     }
-     else {
+    else {
       servoBlue.write(0);
     }
     if (greenState == HIGH) {
       servoGreen.write(pos);
       delay(smallDelay);
     }
-     else {
+    else {
       servoGreen.write(0);
     }
   }
